c6a5389c48d4fbfd299bad6fd85de8223404cda7
Ehsan <eaehsanea@gmail.com>
2025-03-15 20:39:41 +0330

    - Add CountryRepository interface - Add dartz package import for Either type

---
+++ lib/src/domain/repositories/country_repository.dart
@@ -0,0 +1,7 @@
+import 'package:dartz/dartz.dart';
+import '../../core/error/failures.dart';
+import '../entities/country.dart';
+
+abstract class CountryRepository {
+  Future<Either<Failure, List<Country>>> getCountries();
+}
