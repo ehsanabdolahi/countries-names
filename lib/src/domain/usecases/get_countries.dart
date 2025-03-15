9020848e05b07c28458ef175ab7f6bfe04030e67
Ehsan <eaehsanea@gmail.com>
2025-03-15 20:42:09 +0330

    Add GetCountries usecase

---
+++ lib/src/domain/usecases/get_countries.dart
@@ -0,0 +1,18 @@
+import 'package:dartz/dartz.dart';
+import '../../core/error/failures.dart';
+import '../entities/country.dart';
+import '../repositories/country_repository.dart';
+
+class GetCountries {
+  final CountryRepository repository;
+
+  GetCountries(this.repository);
+
+  Future<Either<Failure, List<Country>>> execute() {
+    return repository.getCountries();
+  }
+
+  Future<Either<Failure, List<Country>>> call() {
+    return repository.getCountries();
+  }
+}
