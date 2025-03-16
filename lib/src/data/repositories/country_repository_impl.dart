2a12f74aad658e657037d2c5f75bf8ab4294aad2
Ehsan <eaehsanea@gmail.com>
2025-03-16 07:12:08 +0330

    Add CountryRepositoryImpl for repository implementation

---
+++ lib/src/data/repositories/country_repository_impl.dart
@@ -0,0 +1,35 @@
+import 'package:dartz/dartz.dart';
+import '../../core/error/exceptions.dart';
+import '../../core/error/failures.dart';
+import '../../core/network/network_info.dart';
+import '../../domain/entities/country.dart';
+import '../../domain/repositories/country_repository.dart';
+import '../datasources/country_remote_data_source.dart';
+import '../models/country_model_extension.dart';
+
+class CountryRepositoryImpl implements CountryRepository {
+  final CountryRemoteDataSource remoteDataSource;
+  final NetworkInfo networkInfo;
+
+  CountryRepositoryImpl({
+    required this.remoteDataSource,
+    required this.networkInfo,
+  });
+
+  @override
+  Future<Either<Failure, List<Country>>> getCountries() async {
+    if (await networkInfo.isConnected) {
+      try {
+        final countryModels = await remoteDataSource.getCountries();
+        final countries = countryModels.map((model) => model.toEntity()).toList();
+        return Right(countries);
+      } on ServerException catch (e) {
+        return Left(ServerFailure(message: e.message));
+      } catch (e) {
+        return Left(ServerFailure(message: e.toString()));
+      }
+    } else {
+      return const Left(NetworkFailure(message: 'No internet connection'));
+    }
+  }
+}
